5f0d6de86ab50efd4df029b388cb61778cdb7c5b
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-02-28 22:01:03 -0500

    Added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheesburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("InNOut Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", false);
