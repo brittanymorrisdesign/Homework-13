a638a062af38ef7c1d82c80389a31686aefd1fe1
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-03-02 00:12:16 -0500

    Added partials

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,7 +1,7 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   // Change devour state on click of .change-devour
-  $('change-devoured').on('click', function(event) {
+  $('.change-devour').on('click', function(event) {
     const id = $(this).data('id');
     const newDevour = $(this).data('newdevour');
 
@@ -25,7 +25,7 @@ $(function() {
     event.preventDefault();
 
     const newBurger = {
-      name: $('#burger')
+      name: $('#br')
         .val()
         .trim(),
       devoured: $('[name=devoured]:checked')
@@ -43,18 +43,18 @@ $(function() {
       location.reload();
     });
   });
-});
 
-// Delete selected burger
-$('.delete-burger').on('click', function(event) {
-  const id = $(this).data('id');
+  // Delete selected burger
+  $('.delete-burger').on('click', function(event) {
+    const id = $(this).data('id');
 
-  // Send the DELETE request.
-  $.ajax(`/api/burgers/${id}`, {
-    type: 'DELETE',
-  }).then(function() {
-    console.log('deleted burger', id);
-    // Reload the page to get the updated list
-    location.reload();
+    // Send the DELETE request.
+    $.ajax(`/api/burgers/${id}`, {
+      type: 'DELETE',
+    }).then(function() {
+      console.log('deleted burger', id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 });
